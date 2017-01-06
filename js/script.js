f9cbf047255f89b1bbc1f5a1e81d8788846ad060
felipeacelino <felipeacelino@hotmail.com>
2017-01-06 00:08:53 -0200

    Fix ajustes

--- js/script.js
+++ js/script.js
@@ -132,7 +132,7 @@
 				front.setAttribute('class', 'front');
 
 				var imgFront = doc.createElement('img');
-				imgFront.setAttribute('src', 'images/question.jpg');
+				imgFront.setAttribute('src', 'images/playboy.jpg');
 				front.appendChild(imgFront);
 				card.appendChild(front);
 
@@ -261,9 +261,8 @@
 
 	}
 
-	//var images = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg'];
-	var images = ['1.jpg','2.jpg','3.jpg'];
-	
+	var images = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg'];
+		
 	var game = new Game(images);
 	
 	game.createCards();
